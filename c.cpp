9a7ebdf321408d1aa9f05bb6e242940a69d5875d
aryanjamwal01 <jamwalaryan84@gmail.com>
2024-02-18 21:26:10 +0530

    added c.cpp

---
+++ c.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    int z;
+    cout<<"First Element\n";
+    cin>>x;
+    cout<<"Second Element\n";
+    cin>>y;
+    z=x*y;
+    cout<<"The product is="<<z;
+}
